7b94f17a6bc6eed6bf5182988a36869bc2b9df7b
ranjithal2000 <98078380+ranjithal2000@users.noreply.github.com>
2022-02-07 17:00:13 +0530

    Create secondLargest.py

---
+++ secondLargest.py
@@ -0,0 +1,8 @@
+#taking user input
+# list=[]
+# n=int(input("enter the number of elements:"))
+# for i in range(1,n+1):
+#     b=int(input("enter elements:"))
+#     list.append(b)
+# list.sort()
+# print("second largest is:",list[-2])
