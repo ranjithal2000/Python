ac48771a7d1ec2b288c3a12018f42cf3552a43ce
ranjithal2000 <98078380+ranjithal2000@users.noreply.github.com>
2022-02-07 17:03:59 +0530

    Create tupleList.py

---
+++ tupleList.py
@@ -0,0 +1,25 @@
+val=['L','Ranju','Gowda']
+for i in range(len(val)):
+    print("Ranjitha",val[i],".")
+
+dig=[1,4,8,2,5]
+for i in dig:
+    print(i,end=" ")
+else:
+    print("no items")
+
+tuple=(1,"ranju","bee",2+2)
+print(tuple[3])
+
+i=1
+while(i<5):
+    print(i,end=" ")
+    i +=1
+
+L=[1,"ran","ju",2000]
+for i in L :
+    print(i)
+L.append(3)
+print(L)
+L.pop()
+print(L)
