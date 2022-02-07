c0fb77261da760c76dc352df898091076575e640
ranjithal2000 <98078380+ranjithal2000@users.noreply.github.com>
2022-02-07 16:49:06 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,13 @@
+print("hello world")
+a=10
+b=45.6
+print(a)
+print(b)
+c="Ranjitha L"
+print(c)
+print(c[0:9])
+print(c[-1])
+print(c * 2)
+print(c + ' ' + "Gowda")
+list=("ranju",3,2000,5.1)
+print(list)
